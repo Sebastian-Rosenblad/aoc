d4226169ffbbb13126a4a40427b7bffb7ff789b2
SebastianRosenblad-Voyado <sebastian.rosenblad@voyado.com>
2025-12-01 20:34:31 +0100

    style: make stars look nicer

--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -5,8 +5,8 @@ interface Props {
 }
 
 export default function Stars({ status }: Props) {
-  return <div className='stars'>
-    <span className={status === 'silver' ? 'silver' : status === 'gold' ? 'gold' : ''}>*</span>
-    <span className={status === 'gold' ? 'gold' : ''}>*</span>
-  </div>;
+  return <section className='stars'>
+    <div className={status === 'silver' ? 'silver star' : status === 'gold' ? 'gold star' : 'star'}>*</div>
+    <div className={status === 'gold' ? 'gold star' : 'star'} style={{left: '16px'}}>*</div>
+  </section>;
 }
