bb2dff6e0b5caf1fb7a26210e1b83a47886fd704
SebastianRosenblad-Voyado <sebastian.rosenblad@voyado.com>
2025-12-04 19:43:23 +0100

    feat: solve day 1 year 2022

--- src/challenges/years/2022/day01.tsx
+++ src/challenges/years/2022/day01.tsx
@@ -2,24 +2,39 @@ import Template from '../../../components/Template.tsx';
 import type { Meta } from '../../types';
 import { real, example } from './day01.data.ts';
 
-export const meta: Meta = { year: 2022, day: 1, status: 'other' };
+export const meta: Meta = { year: 2022, day: 1, status: 'gold' };
 
 export default function Day01() {
   function parse(input: string) {
-    return;
+    const lines = input.split('\n');
+    const calories: number[][] = [[]];
+    let i = 0;
+    for (let j = 0; j < lines.length; j++) {
+      if (lines[j].trim() === '') {
+        i++;
+        calories.push([]);
+        continue;
+      }
+      calories[i].push(parseInt(lines[j].trim(), 10));
+    }
+    return calories;
   }
 
   function one(input: string): string {
     const data = parse(input);
-    console.log(data);
-    let solution = 0;
+    let solution = data
+      .map(elf => elf.reduce((a, b) => a + b, 0))
+      .sort((a, b) => b - a)[0];
     return solution.toString();
   }
 
   function two(input: string): string {
     const data = parse(input);
-    console.log(data);
-    let solution = 0;
+    let solution = data
+      .map(elf => elf.reduce((a, b) => a + b, 0))
+      .sort((a, b) => b - a)
+      .slice(0, 3)
+      .reduce((a, b) => a + b, 0);
     return solution.toString();
   }
 
