31a958143b9fe91e84c27784afeae6c9f5e68a22
SebastianRosenblad-Voyado <sebastian.rosenblad@voyado.com>
2025-12-13 17:41:34 +0100

    feat: solve day 16 year 2023

--- src/challenges/years/2023/day16.tsx
+++ src/challenges/years/2023/day16.tsx
@@ -2,119 +2,130 @@ import Template from '../../../components/Template.tsx';
 import type { Meta } from '../../types';
 import { real, example } from './day16.data.ts';
 
-export const meta: Meta = { year: 2023, day: 16, status: 'other', times: { one: 8271, two: 9250 } };
+export const meta: Meta = { year: 2023, day: 16, status: 'gold', times: { one: 8271, two: 9250 } };
 
-interface CoordsM {
+type Item = '|' | '-' | '\\' | '/';
+
+interface v2d {
   x: number;
   y: number;
 }
 
-interface BeamM {
-  x: number;
-  y: number;
-  dir: CoordsM;
-  key: string;
+interface Beam {
+  pos: v2d;
+  dir: v2d;
 }
 
-interface ItemM {
-  x: number;
-  y: number;
-  type: string;
-}
 
 export default function Day16() {
   function parse(input: string) {
-    return input.split(/\r?\n/);
+    const lines = input.trim().split('\n');
+    return lines;
   }
 
   function one(input: string): string {
-    return calculate(parse(input), true);
+    const data = parse(input);
+    return getTotalEnergized({ pos: { x: 0, y: 0 }, dir: { x: 1, y: 0 } }, data).toString();
   }
 
   function two(input: string): string {
-    return calculate(parse(input), false);
-  }
-
-  let width: number = 0, height: number = 0;
-  let sims: number = 0;
-
-  function calculate(a: Array<string>, partOne: boolean): string {
-    width = a[0].length; height = a.length;
-    let items: Array<ItemM> = [];
-    for (let x = 0; x < width; x++) for (let y = 0; y < height; y++) if (a[y][x] !== ".") items.push({ x: x, y: y, type: a[y][x] });
-    if (partOne)
-      return simulate(beamify(0, 0, 1, 0), items).toString();
-    let starts: Array<BeamM> = [];
-    for (let x = 0; x < width; x++) { starts.push(beamify(x, 0, 0, 1)); starts.push(beamify(x, height - 1, 0, -1)); }
-    for (let y = 0; y < height; y++) { starts.push(beamify(0, y, 1, 0)); starts.push(beamify(width - 1, y, -1, 0)); }
-    sims = 0;
-    let final: Array<number> = starts.map(start => simulate(start, items));
-    return Math.max(...final).toString();
-  }
-
-  function simulate(start: BeamM, items: Array<ItemM>): number {
-    console.log((sims++) + "/" + (width + height) * 2);
-    let beams: Array<BeamM> = [start];
-    let item: ItemM | undefined = items.find(i => i.x === start.x && i.y === start.y);
-    if (item) beams = turn(beams[0], item.type);
-    let history: Array<CoordsM> = move(beams, items, []).map(h => {
-      return {
-        x: parseInt(h.split(",")[0]),
-        y: parseInt(h.split(",")[1])
-      };
-    });
-    return history.filter((h, i) => history.findIndex(n => n.x === h.x && n.y === h.y) === i).length;
-  }
-
-  function move(beams: Array<BeamM>, items: Array<ItemM>, history: Array<string>): Array<string> {
-    if (beams.length > 0) {
-      let beam: BeamM = beams[0];
-      if (history.includes(beam.key)) return move(beams.slice(1), items, history);
-      let item: ItemM | undefined = undefined;
-      if (beam.dir.x < 0) {
-        let temp = items.filter(i => i.x < beam.x && i.y === beam.y);
-        if (temp.length > 0) item = temp[temp.length - 1];
-      }
-      else if (beam.dir.x > 0) {
-        let temp = items.filter(i => i.x > beam.x && i.y === beam.y);
-        if (temp.length > 0) item = temp[0];
-      }
-      else if (beam.dir.y < 0) {
-        let temp = items.filter(i => i.x === beam.x && i.y < beam.y);
-        if (temp.length > 0) item = temp[temp.length - 1];
-      }
-      else if (beam.dir.y > 0) {
-        let temp = items.filter(i => i.x === beam.x && i.y > beam.y);
-        if (temp.length > 0) item = temp[0];
-      }
-      let length: number = 0;
-      if (!item) {
-        if (beam.dir.x < 0) length = beam.x + 1;
-        else if (beam.dir.x > 0) length = width - beam.x;
-        else if (beam.dir.y < 0) length = beam.y + 1;
-        else if (beam.dir.y > 0) length = height - beam.y;
-      }
-      else length = Math.abs(item.x - beam.x + item.y - beam.y);
-      for (let i = 0; i < length; i++) history.push((beam.x + beam.dir.x * i) + "," + (beam.y + beam.dir.y * i) + "," + beam.dir.x + "," + beam.dir.y);
-      if (!item) return move(beams.slice(1), items, history);
-      return move([...turn(beamify(item.x, item.y, beam.dir.x, beam.dir.y), item.type), ...beams.slice(1)], items, history);
+    const data = parse(input);
+    let solution = 0;
+    for (let x = 0; x < data[0].length; x++) {
+      const total = Math.max(
+        getTotalEnergized({ pos: { x, y: 0 }, dir: { x: 0, y: 1 } }, data),
+        getTotalEnergized({ pos: { x, y: data.length - 1 }, dir: { x: 0, y: -1 } }, data),
+      );
+      if (total > solution) solution = total;
     }
-    return history;
-  }
-
-  function turn(beam: BeamM, type: string): Array<BeamM> {
-    if (type === "/") return [beamify(beam.x, beam.y, -beam.dir.y, -beam.dir.x)];
-    if (type === "L") return [beamify(beam.x, beam.y, beam.dir.y, beam.dir.x)];
-    if (type === "|") {
-      if (beam.dir.x === 0) return [beam];
-      return [beamify(beam.x, beam.y, 0, -1), beamify(beam.x, beam.y, 0, 1)];
+    for (let y = 0; y < data.length; y++) {
+      const total = Math.max(
+        getTotalEnergized({ pos: { x: 0, y }, dir: { x: 1, y: 0 } }, data),
+        getTotalEnergized({ pos: { x: data[0].length - 1, y }, dir: { x: -1, y: 0 } }, data),
+      );
+      if (total > solution) solution = total;
     }
-    if (beam.dir.y === 0) return [beam];
-    return [beamify(beam.x, beam.y, -1, 0), beamify(beam.x, beam.y, 1, 0)];
+    return solution.toString();
   }
 
-  function beamify(x: number, y: number, dx: number, dy: number): BeamM {
-    return { x: x, y: y, dir: { x: dx, y: dy }, key: x + "," + y + "," + dx + "," + dy };
+  function getTotalEnergized(beam: Beam, map: string[]) {
+    let paths = findAllPaths(beam, map);
+    let positions: Set<string> = new Set();
+    for (const key in paths)
+      for (const pos of paths[key])
+        positions.add(`${pos.x},${pos.y}`);
+    return positions.size;
+  }
+
+  function findAllPaths(start: Beam, map: string[]) {
+    const paths: Record<string, v2d[]> = {};
+    let beamsToCheck = [start];
+    while (beamsToCheck.length > 0) {
+      const beam = beamsToCheck.shift()!;
+      const key = toKey(beam);
+      if (paths[key]) continue;
+      const { path, next } = findPath(beam, map);
+      beamsToCheck = [...beamsToCheck, ...next];
+      paths[key] = path;
+    }
+    return paths;
+  }
+
+  function findPath(start: Beam, map: string[]) {
+    const path = [start.pos];
+    let pos: v2d = { x: start.pos.x, y: start.pos.y };
+    while (map[pos.y][pos.x] === '.') {
+      pos.x += start.dir.x;
+      pos.y += start.dir.y;
+      if (pos.x < 0 || pos.y < 0 || pos.x >= map[0].length || pos.y >= map.length) return { path, next: [] };
+      path.push({ x: pos.x, y: pos.y });
+    }
+    const next = collideResult({ pos, dir: start.dir }, map[pos.y][pos.x] as Item)
+      .filter(beam => beam.pos.x >= 0 && beam.pos.y >= 0 && beam.pos.x < map[0].length && beam.pos.y < map.length);
+    return { path, next };
+  }
+
+  function collideResult(beam: Beam, item: Item) {
+    switch (item) {
+      case '|': return splitVertical(beam);
+      case '-': return splitHorizontal(beam);
+      case '/': return mirrorForward(beam);
+      default: return mirrorBackward(beam);
+    }
+  }
+
+  function splitVertical(beam: Beam) {
+    if (beam.dir.y !== 0)
+      return [{ pos: { x: beam.pos.x, y: beam.pos.y + beam.dir.y }, dir: beam.dir }] as Beam[];
+    return [
+      { pos: { x: beam.pos.x, y: beam.pos.y + 1 }, dir: { x: 0, y: 1 } },
+      { pos: { x: beam.pos.x, y: beam.pos.y - 1 }, dir: { x: 0, y: -1 } },
+    ] as Beam[];
+  }
+  
+  function splitHorizontal(beam: Beam) {
+    if (beam.dir.x !== 0)
+      return [{ pos: { x: beam.pos.x + beam.dir.x, y: beam.pos.y }, dir: beam.dir }] as Beam[];
+    return [
+      { pos: { x: beam.pos.x + 1, y: beam.pos.y }, dir: { x: 1, y: 0 } },
+      { pos: { x: beam.pos.x - 1, y: beam.pos.y }, dir: { x: -1, y: 0 } },
+    ] as Beam[];
+  }
+
+  function mirrorForward(beam: Beam) {
+    if (beam.dir.x !== 0)
+      return [{ pos: { x: beam.pos.x, y: beam.pos.y - beam.dir.x }, dir: { x: 0, y: -beam.dir.x } }] as Beam[];
+    return [{ pos: { x: beam.pos.x - beam.dir.y, y: beam.pos.y }, dir: { x: -beam.dir.y, y: 0 } }] as Beam[];
+  }
+
+  function mirrorBackward(beam: Beam) {
+    if (beam.dir.x !== 0)
+      return [{ pos: { x: beam.pos.x, y: beam.pos.y + beam.dir.x }, dir: { x: 0, y: beam.dir.x } }] as Beam[];
+    return [{ pos: { x: beam.pos.x + beam.dir.y, y: beam.pos.y }, dir: { x: beam.dir.y, y: 0 } }] as Beam[];
+  }
+
+  function toKey(beam: Beam) {
+    return `${beam.pos.x},${beam.pos.y},${beam.dir.x},${beam.dir.y}`;
   }
 
   return (
