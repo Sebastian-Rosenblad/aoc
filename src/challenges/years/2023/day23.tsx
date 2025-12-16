8b0c232f41006f60bfffbb9c54282309759e53fb
SebastianRosenblad-Voyado <sebastian.rosenblad@voyado.com>
2025-12-16 13:31:04 +0100

    feat: solve day 23 year 2023 part 2

--- src/challenges/years/2023/day23.tsx
+++ src/challenges/years/2023/day23.tsx
@@ -2,126 +2,173 @@ import Template from '../../../components/Template.tsx';
 import type { Meta } from '../../types';
 import { real, example } from './day23.data.ts';
 
-export const meta: Meta = { year: 2023, day: 23, status: 'other', times: { one: 86400 } };
+export const meta: Meta = { year: 2023, day: 23, status: 'silver', times: { one: 86400 }, note: 'Part 2 real input takes up to 1 and a half minute' };
 
-interface TrailM {
-  start: CoordM;
-  end: CoordM;
-  length: number;
-  splits: Array<CoordM>;
-}
-
-interface CheckM {
-  from: CoordM;
-  start: CoordM;
-}
-
-interface NodeM {
-  id: string;
-  connections: Array<{ id: string; length: number; }>;
-}
-
-interface CoordM {
+interface v2d {
   x: number;
   y: number;
 }
 
-let ids: Array<string> = [];
+interface Junction {
+  pos: v2d;
+  paths: Path[];
+}
 
-function GetID(): string {
-  let newid: string;
-  do { newid = Math.random().toString(16).slice(2, 5); } while (ids.includes(newid));
-  return newid;
+interface Path {
+  out: v2d;
+  len: number;
+  dest: v2d;
 }
 
 export default function Day23() {
+  let map: string[];
+  let ice: boolean;
+
   function parse(input: string) {
-    return input.split(/\r?\n/);
+    map = input.trim().split('\n');
+    return { start: { x: map[0].indexOf('.'), y: 0 } as v2d, end: { x: map[map.length - 1].indexOf('.'), y: map.length - 1 } as v2d };
   }
 
   function one(input: string): string {
-    return calculate(parse(input), true);
+    const { start, end } = parse(input);
+    ice = true;
+    return getLongestHikeLength(start, end).toString();
   }
 
   function two(input: string): string {
-    return calculate(parse(input), false);
+    const { start, end } = parse(input);
+    ice = false;
+    return getLongestHikeLength(start, end).toString();
   }
 
-  function calculate(a: Array<string>, partOne: boolean): string {
-    /*const map: Array<Array<string>> = a.map(line => line.split(""));
-    const startx: number = map[0].indexOf(".");
-    let trails: Array<TrailM> = [], checks: Array<CheckM> = [{ from: { x: startx, y: 0 }, start: { x: startx, y: 1 } }];
-    while (checks.length > 0) {
-      const check: CheckM | undefined = checks.shift();
-      if (check !== undefined && !trails.find(trail => Equals(trail.start, check.start))) {
-        const newTrail: TrailM = FindTrail(check.from, check.start, map, partOne);
-        trails.push(newTrail);
-        newTrail.splits.forEach(split => checks.push({ from: newTrail.end, start: split }));
+  function getLongestHikeLength(startPos: v2d, endPos: v2d) {
+    const junctions = [{
+      pos: startPos,
+      paths: [{
+        out: { x: startPos.x, y: startPos.y + 1 },
+        len: -1,
+        dest: { x: -1, y: -1 }
+      }]
+    }, ...getJunctions()];
+    calculateJunctionPaths(junctions, endPos);
+    //console.log(junctions);
+    return bruteForceSearch(junctions, junctions[0], [], endPos, 0, 0);
+  }
+
+  function bruteForceSearch(all: Junction[], current: Junction, seen: string[], end: v2d, length: number, max: number): number {
+    const key = current.pos.x + ',' + current.pos.y;
+    if (seen.includes(key)) return max;
+    let newMax = max;
+    for (const path of current.paths) {
+      if (isEquals(path.dest, end)) {
+        newMax = Math.max(newMax, length + path.len);
+        continue;
       }
+      const pathLength = bruteForceSearch(
+        all,
+        all.find(j => isEquals(j.pos, path.dest))!,
+        [...seen, key],
+        end,
+        length + path.len,
+        newMax
+      );
+      if (pathLength > newMax) newMax = pathLength;
     }
-    const minlength: number = trails.filter(trail => trail.splits.length === 0).reduce((a, b) => a + b.length, 0);
-    const nodes: Array<NodeM> = GetNodes(trails.filter(trail => !Equals(trail.end, { x: startx, y: 0 })));
-    trails.forEach(trail => trail.splitTrails = trail.splits.map(coord =>  trails.find(t => Equals(t.start, coord)) || { id: "-1" } as TrailM).filter(t => t.id !== "-1"));
-    let trecks: Array<Array<TrailM>> = [[trails[0]]];
-    let completed: Array<Array<TrailM>> = [];
-    let count: number = 0;
-    let longest: number = 0;
-    while (trecks.length > 0) {
-      let treck: Array<TrailM> | undefined = trecks.shift();
-      if (treck !== undefined) {
-        let splits: Array<TrailM> = treck[treck.length - 1].splitTrails;
-        if (splits.length === 0) {
-          completed.push(treck);
-          if (treck.reduce((a, b) => a + b.length, 0) > longest) {
-            longest = treck.reduce((a, b) => a + b.length, 0);
+    return newMax;
+  }
+
+  function getJunctions() {
+    let junctions: Junction[] = [];
+    for (let j = 1; j < map.length - 1; j++) {
+      for (let i = 1; i < map[0].length - 1; i++) {
+        if (map[j][i] === '#') continue;
+        let _in: v2d[] = [], _out: v2d[] = [];
+        const directions: v2d[] = [{ x: -1, y: 0 }, { x: 1, y: 0 }, { x: 0, y: -1 }, { x: 0, y: 1 }];
+        for (const dir of directions) {
+          const pos = add({ x: i, y: j }, dir);
+          if (map[pos.y][pos.x] === '.') _out.push(pos);
+          else if (map[pos.y][pos.x] !== '#') {
+            if (canTraverse(map[pos.y][pos.x], dir)) _out.push(pos);
+            else _in.push(pos);
           }
         }
-        for (let i = 0; i < splits.length; i++) {
-          if (partOne) trecks.push([...treck, splits[i]]);
-          else if (!treck.map(trail => trail.end).find(coord => Equals(coord, splits[i].end))) trecks.push([...treck, splits[i]]);
+        if (!ice) { _out = _out.concat(_in); _in = []; }
+        if (_out.length >= 3 || (_out.length >= 2 && _in.length > 0)) {
+          const paths = _out.map(o => ({
+            out: o,
+            len: -1,
+            dest: { x: -1, y: -1 }
+          }));
+          junctions.push({ pos: { x: i, y: j }, paths });
         }
       }
     }
-    return Math.max(...completed.map(treck => treck.reduce((a, b) => a + b.length, 0))).toString();*/
-    return "0";
+    return junctions;
   }
 
-  function FindTrail(from: CoordM, start: CoordM, map: Array<Array<string>>, partOne: boolean): TrailM {
-    let walked: Array<CoordM> = [from], nexts: Array<CoordM> = [start];
-    while (nexts.length === 1) {
-      const next: CoordM = nexts[0];
-      nexts = [];
-      for (let i = 0; i < 4; i++) {
-        const test: CoordM = { x: next.x + (2 - i) % 2, y: next.y + (i - 1) % 2 };
-        if (Equals(test, walked[walked.length - 1])) continue;
-        if (test.y >= map.length || test.y < 0) continue;
-        if (map[test.y][test.x] === "#") continue;
-        if (partOne && map[test.y][test.x] === ">" && i !== 1) continue;
-        if (partOne && map[test.y][test.x] === "v" && i !== 2) continue;
-        nexts.push(test);
+  function calculateJunctionPaths(junctions: Junction[], end: v2d) {
+    for (const junction of junctions) {
+      for (const path of junction.paths) {
+        if (path.len !== -1) continue;
+        const steps = getSteps(path.out, junction.pos);
+        const lastStep = steps[steps.length - 1];
+        path.len = steps.length;
+        path.dest = lastStep;
       }
-      walked.push(next);
     }
-    return {
-      start: start,
-      end: walked[walked.length - 1],
-      length: walked.length - 1,
-      splits: nexts
-    };
   }
 
-  function GetNodes(trails: Array<TrailM>): Array<NodeM> {
-    let uniques: Array<CoordM> = trails.map(trail => trail.end);
-    uniques = uniques.filter((unique, i) => uniques.findIndex(u => Equals(unique, u)) === i);
-    let nodes: Array<NodeM> = [];
-    uniques.forEach(unique => {
-      const connections: Array<TrailM> = trails.filter(trail => Equals(unique, trail.end));
-    });
-    return nodes;
+  function getSteps(from: v2d, skip?: v2d) {
+    const steps = [from];
+    let neighbors = findNeighbors(from, skip);
+    let prev = from;
+    while (neighbors.length === 1) {
+      const current = neighbors[0];
+      steps.push(current);
+      neighbors = findNeighbors(current, prev);
+      prev = current;
+    }
+    return steps;
   }
 
-  let Equals = (a: CoordM, b: CoordM): boolean => a.x === b.x && a.y === b.y;
+  function findNeighbors(origin: v2d, skip?: v2d) {
+    let steps: v2d[] = [];
+    const directions: v2d[] = [{ x: -1, y: 0 }, { x: 1, y: 0 }, { x: 0, y: -1 }, { x: 0, y: 1 }];
+    for (const dir of directions) {
+      const pos = add(origin, dir);
+      if (
+        isWithinBounds(pos) &&
+        canTraverse(map[pos.y][pos.x], dir) &&
+        !isEquals(pos, skip)
+      ) steps.push(pos);
+    }
+    return steps;
+  }
 
+  function canTraverse(terrain: string, dir: v2d) {
+    if (terrain === '.') return true;
+    if (terrain === '#') return false;
+    if (!ice) return true;
+    if (terrain === '<' && dir.x === -1) return true;
+    if (terrain === '>' && dir.x === 1) return true;
+    if (terrain === '^' && dir.y === -1) return true;
+    if (terrain === 'v' && dir.y === 1) return true;
+    return false;
+  }
+
+  function isWithinBounds(pos: v2d) {
+    return pos.x >= 0 && pos.x < map[0].length && pos.y >= 0 && pos.y < map.length;
+  }
+
+  function isEquals(a: v2d, b?: v2d) {
+    if (!b) return false;
+    return a.x === b.x && a.y === b.y;
+  }
+
+  function add(a: v2d, b: v2d): v2d {
+    return { x: a.x + b.x, y: a.y + b.y };
+  }
+  
   return (
     <Template
       meta={meta}
